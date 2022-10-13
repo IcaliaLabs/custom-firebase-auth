7d9a3d24f15503c2223b02dcff6b66eeaa8e5e14
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-10-13 18:30:19 -0500

    Creating views and rails command to generate them.

--- lib/generators/icalialabs_firebase/icalialabs_firebase_generator.rb
+++ lib/generators/icalialabs_firebase/icalialabs_firebase_generator.rb
@@ -1,53 +1,14 @@
 class IcalialabsFirebaseGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path("templates", __dir__)
+  source_root File.expand_path("../../../", __dir__)
 
   def create_views
     if class_name == 'Views'
-      create_file "app/views/firebase/registrations.html.erb", <<-FILE
-<div>
-  <h2>Log in</h2>
-  <%= form_with(url: '#', method: :post) do |f| %>
-    <div>
-      <%= f.label :email %>
-      <%= f.text_field :email %>
-    </div>
-
-    <div>
-      <%= f.label :password %>
-      <%= f.password_field :password %>
-    </div>
-
-    <div>
-      <%= f.submit 'Log in' %>
-    </div>
-  <% end %>
-</div>
-      FILE
-
-      create_file "app/views/firebase/sessions.html.erb", <<-FILE
-<div>
-  <h2>Sign in</h2>
-  <%= form_with(url: '#', method: :post) do |f| %>
-    <div>
-      <%= f.label :email %>
-      <%= f.text_field :email %>
-    </div>
-
-    <div>
-      <%= f.label :password %>
-      <%= f.password_field :password %>
-    </div>
-
-    <div>
-      <%= f.submit 'Sign in' %>
-    </div>
-  <% end %>
-</div>
-      FILE
+      directory 'app/views/firebase/', 'app/views/firebase/'
     elsif class_name == "User"
-      template 'user.rb', File.join('app/models', class_path, "user.rb")
       time = Time.now.strftime("%Y%m%d%H%M%S")
-      template "migration.rb", File.join('db/migrate/', class_path, "#{time}_create_users.rb")
+
+      template 'lib/generators/icalialabs_firebase/templates/user.rb', File.join('app/models', class_path, "user.rb")
+      template "lib/generators/icalialabs_firebase/templates/migration.rb", File.join('db/migrate/', class_path, "#{time}_create_users.rb")
     end
   end
 end
