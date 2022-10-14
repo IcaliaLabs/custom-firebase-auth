b5771693a95aab65baaed5a5877556e148c4e3c6
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-10-13 19:16:44 -0500

    Creating session controller and firebase initializer.

---
+++ lib/generators/icalialabs_firebase/templates/icalialabs_firebase.rb
@@ -0,0 +1,4 @@
+# Within this file you can set a bunch of configuration to use this gem
+
+# Here in this section, you will paste the API KEY provided from Google Firebase.
+# IcalialabsFirebase.firebase_api_key = "MY_KEY"
