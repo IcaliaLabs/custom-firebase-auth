6424afd7b49237e9ffed006b590cba4ff78aa1ef
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-10-13 19:15:44 -0500

    Generating intializer and views with their own folders.

--- lib/generators/icalialabs_firebase/icalialabs_firebase_generator.rb
+++ lib/generators/icalialabs_firebase/icalialabs_firebase_generator.rb
@@ -2,13 +2,17 @@ class IcalialabsFirebaseGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("../../../", __dir__)
 
   def create_views
-    if class_name == 'Views'
+    if class_name == 'Install'
+      copy_file "lib/generators/icalialabs_firebase/templates/icalialabs_firebase.rb", "config/initializers/icalialabs_firebase.rb"
+    elsif class_name == 'Views'
       directory 'app/views/firebase/', 'app/views/firebase/'
-    elsif class_name == "User"
-      time = Time.now.strftime("%Y%m%d%H%M%S")
-
-      template 'lib/generators/icalialabs_firebase/templates/user.rb', File.join('app/models', class_path, "user.rb")
-      template "lib/generators/icalialabs_firebase/templates/migration.rb", File.join('db/migrate/', class_path, "#{time}_create_users.rb")
     end
+
+    # elsif class_name == "User"
+    #   time = Time.now.strftime("%Y%m%d%H%M%S")
+
+    #   template 'lib/generators/icalialabs_firebase/templates/user.rb', File.join('app/models', class_path, "user.rb")
+    #   template "lib/generators/icalialabs_firebase/templates/migration.rb", File.join('db/migrate/', class_path, "#{time}_create_users.rb")
+    # end
   end
 end
